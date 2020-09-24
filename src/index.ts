bf514163c82e7eb84a6d535b7afda9266cf80255
tfmcmahon <tfmcmahon@gmail.com>
2020-09-24 11:00:32 -0500

    post, graphQL, basic CRUD

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,31 @@
+import 'reflect-metadata'
 import { MikroORM } from '@mikro-orm/core'
-const config = require('config')
+import { Post } from './entities/Post'
+import microConfig from './mikro-orm.config'
+import express from 'express'
+import { ApolloServer } from 'apollo-server-express'
+import { buildSchema } from 'type-graphql'
+import { HelloResolver } from './resolvers/hello'
+import { PostResolver } from './resolvers/post'
 
 const main = async () => {
-  const orm = MikroORM.init({
-    dbName: 'lireddit',
-    user: 'postgres',
-    password: config.postgreSQL,
-    debug: process.env.NODE_ENV !== 'production',
-    type: 'postgresql',
+  const orm = await MikroORM.init(microConfig)
+  await orm.getMigrator().up()
+
+  const app = express()
+
+  const apolloServer = new ApolloServer({
+    schema: await buildSchema({
+      resolvers: [HelloResolver, PostResolver],
+      validate: false,
+    }),
+    context: () => ({ em: orm.em }),
+  })
+
+  apolloServer.applyMiddleware({ app })
+
+  app.listen(4000, () => {
+    console.log('server start on 4000')
   })
 }
 
